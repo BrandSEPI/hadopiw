ed3b521a7f9a6008278c7c824b7784372b9cb1b0
BrandSZer <brands38@yahoo.com>
2022-05-24 06:45:23 +0200

    début front

--- front_hadopiw/src/App.vue
+++ front_hadopiw/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar">
+        <router-link to="/"><img class="navLogo" src="./assets/hadopiw.png" alt="HadopiwLogo"></router-link>
+        <router-link to="/tabs"><img class="navItem" src="./assets/kamas.png" alt="TabLogo"></router-link>
+        <router-link to="/connect"><img class="navItem right" src="./assets/kamas.png" alt="TabLogo"></router-link>
+  </nav>
+
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +24,25 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.navbar{
+  gap:30px;
+  width: 100%;
+  height: 75px;
+  display: flex;
+  background-color: rgb(26, 26, 27);
+  align-items: center;
+
+}
+.navLogo{
+  width: 50px;
+  border-radius: 30px;
+  border: solid 3px rgba(5, 5, 5, 0.342)}
+.navItem{
+  width: 35px;
+}
+.right{
+  margin-right: auto;
 }
 </style>
---
+++ front_hadopiw/src/views/homeView.vue
@@ -0,0 +1,20 @@
+<template class="background">
+
+
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  
+}
+</script>
+
+
+
+<style scoped>
+.background{
+    background-color: black;
+}
+</style>
